f64ec247809ca662650d0f236f195c8722d66366
star8041 <kms07001@gmail.com>
2024-10-12 22:07:23 +0900

    오디오, UI (UI 변경 필요)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     [Header("# Game Control")]
+    public bool isLive;
     public float gameTime;
     public float maxGameTime = 2 * 10f;
     [Header("# Player Info")]
-    public int health;
-    public int maxhealth;
+    public float health;
+    public float maxhealth;
     public int level;
     public int kill;
     public int exp;
@@ -19,37 +21,106 @@ public class GameManager : MonoBehaviour
     [Header("# Game Object")]
     public PoolManager poolManager;
     public Player player;
+    public LevelUp uiLevelUp;
+    public Result uiResult;
+    public GameObject enemyCleaner;
 
     private void Awake()
     {
         instance = this;
     }
 
-    private void Start()
+    public void GameStart()
     {
         health = maxhealth;
 
+        //임시 스크립트 (첫 선택)
+        uiLevelUp.Select(0);
+        //isLive = true;
+        Resume();
+
+        AudioManager.instance.PlayBgm(true);
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.Select);
+    }
+
+    public void GameOver()
+    {
+        StartCoroutine(GameOverRoutine());
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        isLive = false;
+        yield return new WaitForSeconds(0.5f);
+
+        uiResult.gameObject.SetActive(true);
+        uiResult.Lose();
+        Stop();
+
+        AudioManager.instance.PlayBgm(false);
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.Lose);
+    }
+
+    public void GameVictory()
+    {
+        StartCoroutine(GameVictoryRoutine());
+    }
+
+    IEnumerator GameVictoryRoutine()
+    {
+        isLive = false;
+        enemyCleaner.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
+        uiResult.gameObject.SetActive(true);
+        uiResult.Win();
+        Stop();
+
+        AudioManager.instance.PlayBgm(false);
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.Win);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Update()
     {
+        if (!isLive)
+            return;
         gameTime += Time.deltaTime;
 
         if (gameTime > maxGameTime)
         {
             gameTime = maxGameTime;
+            GameVictory();
         }
     }
 
     public void GetExp()
     {
+        if (!isLive)
+            return;
         exp++;
 
-        if (exp == nextExp[level])
+        if (exp == nextExp[Mathf.Min(level, nextExp.Length-1)])
         {
             level++;
             exp = 0;
-
+            uiLevelUp.Show();
         }
+    }
+
+    public void Stop()
+    {
+        isLive = false;
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        isLive = true;
+        Time.timeScale = 1;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,8 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if (!GameManager.instance.isLive)
+			return;
 		inputVec.x = Input.GetAxis("Horizontal");
 		inputVec.y = Input.GetAxis("Vertical");
 
@@ -31,17 +33,40 @@ public class Player : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        if (!GameManager.instance.isLive)
+            return;
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
 		// 위치이동
 		rigid.MovePosition(rigid.position + nextVec);
 	}
 
 	private void LateUpdate()
 	{
-		anim.SetFloat("Speed", inputVec.magnitude);
+        if (!GameManager.instance.isLive)
+            return;
+        anim.SetFloat("Speed", inputVec.magnitude);
 		if (inputVec.x != 0)
 		{
 			spriteRenderer.flipX = (inputVec.x < 0);
 		}
 	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		if (!GameManager.instance.isLive)
+			return;
+
+		GameManager.instance.health -= Time.deltaTime * 10;
+
+		if (GameManager.instance.health < 0)
+		{
+			for (int index = 2; index < transform.childCount; index++)
+			{
+				transform.GetChild(index).gameObject.SetActive(false);
+			}
+
+			anim.SetTrigger("Dead");
+			GameManager.instance.GameOver();
+		}
+    }
 }
