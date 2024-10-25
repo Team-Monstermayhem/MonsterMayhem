0e0583c431fd98f6f96b2a1a6b499a00b35d495a
twogarlic <hanle0126@naver.com>
2024-10-25 18:13:21 +0900

    10 웨이브 이후 보스 출현

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,12 +9,14 @@ public class Spawner : MonoBehaviour
 
     int level;
     float timer;
-    float prefabChangeTimer;  
-    List<int> enemyPrefabIndexes;  
-    int currentPrefabIndex;  
-    int currentWave = 1;      
-    float waveDuration = 5f;  
-    int maxWave = 10;         
+    float prefabChangeTimer;
+    List<int> enemyPrefabIndexes;
+    int currentPrefabIndex;
+    int currentWave = 1;
+    float waveDuration = 1f;
+    int maxWave = 10;
+    public GameObject bossPrefab; 
+    private GameObject bossInstance; 
 
     private void Awake()
     {
@@ -23,7 +25,7 @@ public class Spawner : MonoBehaviour
         enemyPrefabIndexes = new List<int>();
         for (int i = 0; i < GameManager.instance.poolManager.prefabs.Length; i++)
         {
-            if (GameManager.instance.poolManager.prefabs[i].CompareTag("Enemy")) 
+            if (GameManager.instance.poolManager.prefabs[i].CompareTag("Enemy"))
             {
                 enemyPrefabIndexes.Add(i);
             }
@@ -32,15 +34,15 @@ public class Spawner : MonoBehaviour
 
     private void Start()
     {
-        currentPrefabIndex = enemyPrefabIndexes[0];  
+        currentPrefabIndex = enemyPrefabIndexes[0];
 
         spawnData = new SpawnData[maxWave];
 
         for (int i = 0; i < maxWave; i++)
         {
-            float spawnTime = Mathf.Max(0.1f, 1.0f - (i * 0.05f)); 
-            int health = 10 + (i * 10); 
-            float speed = 1.0f + (i * 0.1f); 
+            float spawnTime = Mathf.Max(0.1f, 1.0f - (i * 0.05f));
+            int health = 10 + (i * 10);
+            float speed = 1.0f + (i * 0.1f);
             spawnData[i] = new SpawnData { spawnTime = spawnTime, spriteType = i % enemyPrefabIndexes.Count, health = health, speed = speed };
         }
     }
@@ -53,11 +55,10 @@ public class Spawner : MonoBehaviour
         timer += Time.deltaTime;
         prefabChangeTimer += Time.deltaTime;
 
-    
         if (prefabChangeTimer > waveDuration)
         {
             prefabChangeTimer = 0f;
-            NextWave();  
+            NextWave();
         }
 
         level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
@@ -65,7 +66,7 @@ public class Spawner : MonoBehaviour
         if (timer > spawnData[level].spawnTime)
         {
             timer = 0;
-            Spawn();  
+            Spawn();
         }
     }
 
@@ -78,23 +79,23 @@ public class Spawner : MonoBehaviour
             currentPrefabIndex = enemyPrefabIndexes[nextIndex];
             Debug.Log("웨이브 " + currentWave + " 시작!");
         }
-        else
+        else if (currentWave == maxWave && bossInstance == null) 
         {
-            Debug.Log("모든 웨이브 완료!");
+            Debug.Log("Boss is spawning as wave " + maxWave + " has been reached!");
+            bossInstance = Instantiate(bossPrefab, spawnPoint[0].position, Quaternion.identity);
         }
     }
 
     void Spawn()
     {
-        int enemiesToSpawn = currentWave + 1; 
+        int enemiesToSpawn = currentWave + 1;
 
         for (int i = 0; i < enemiesToSpawn; i++)
         {
             GameObject enemy = GameManager.instance.poolManager.GetObject(currentPrefabIndex);
             enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
-            Enemy enemyComponent = enemy.GetComponent<Enemy>();
+            var enemyComponent = enemy.GetComponent<Enemy>();
             enemyComponent.Init(spawnData[level]);
-
             enemyComponent.health = spawnData[level].health + (currentWave * 1.5f);
             enemyComponent.speed = spawnData[level].speed - (currentWave * 0.1f);
         }
